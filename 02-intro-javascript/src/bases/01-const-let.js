d4aaef2a02d7dcde09542b775143600104e02cbf
FranciscoJavierChavez <javi_yankee@icloud.com>
2021-07-18 21:02:36 -0500

    variables y constantes

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+//variables y constantes
+
+const nombre = 'Francisco';
+const apellido = 'chavez';
+
+let valorDado = 5 ;
+valorDado = 4;
+
+console.log(nombre,apellido,valorDado)
+
+if (true)
+{
+    const nombre="Hector";
+    console.log(nombre);
+}
+
+console.log(valorDado);
